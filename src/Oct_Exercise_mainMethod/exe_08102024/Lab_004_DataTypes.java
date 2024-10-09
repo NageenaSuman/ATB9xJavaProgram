4d4bfb960c42dad751708481ef97a045d69a999b
= <=>
2024-10-09 13:05:14 +0800

    today's update

---
+++ src/Oct_Exercise_mainMethod/exe_08102024/Lab_004_DataTypes.java
@@ -0,0 +1,27 @@
+package Oct_Exercise_mainMethod.exe_08102024;
+
+public class Lab_004_DataTypes {
+    public static void main(String[] args) {
+        int a_b = 0; //This group includes byte, short, int, and long, which are for whole-valued signed numbers.
+        int _nageena = 9;
+        int $nageena = 78;
+        // age = 89; --> INvalid
+        int a_ = 123;
+        //int _ = 45; --> _ is keyword and cannot be used alone
+        int $ = 123;
+        System.out.println($);
+        boolean is_nageena_married = true;
+        boolean is$nageena$owns$audi = false;
+        System.out.println(is$nageena$owns$audi);
+        long amountVal = 1234567891l;
+        float intrestRate = 12.25f;
+        double sineVal = 12345.234d;
+        char ch1 = 87; //gets converted to letter
+        char _u = 'Y'; //This group includes char, which represents symbols in a character set, like letters and numbers.
+        System.out.println(intrestRate);
+        System.out.println(ch1);
+        System.out.println(amountVal);
+    }
+
+
+}
---
+++ src/Oct_Exercise_mainMethod/exe_09102024/Lab_005_VariableTypes.java
@@ -0,0 +1,36 @@
+package Oct_Exercise_mainMethod.exe_09102024;
+
+public class Lab_005_VariableTypes {
+    int gg = 98;
+    {
+        //it wont output as it is not under void main
+        System.out.println(gg); // local variable and has scope only within {} and cant be called out of it, there is no concept of global variable in java
+    }
+    public static void main(String[] args) {
+        // Primitive Data Types
+        //byte b = 128; // 8 bits its invalid as it exceeds memory
+        byte b = 127;
+        short s = 3450;
+        long l = 8870511866L;
+        char ch = 'B'; //[A-Z][a-z][0-9]
+        //char name = "Primitive"; //Invalid as it takes '' only single quotes
+        char ch1 = 9;
+        char ch2 = '$';
+        double d = 54.56D;
+        float f = 3.14F;
+        char c2 = '\n'; // Escape Char - give you newline
+        System.out.println(c2);
+        int gg = 98;
+        {
+            System.out.println(gg); // local variable and has scope only within {} and cant be called out of it, there is no concept of global variable in java
+        }
+        char c3 = '\u1F6A'; // Unicode
+        System.out.println(c3);//  Ὢ :)
+        System.out.println(d);
+        System.out.println(ch2);
+        System.out.println(l);
+        System.out.println(b);
+        //System.out.println(gg); invalid local variable
+    }
+
+}
