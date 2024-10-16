66b17d988213cbfd814b0f489387653d7572f436
NageenaSuman <kaunaineee@gmail.com>
2024-10-16 13:46:37 +0800

    master

---
+++ src/Oct_Exercise_mainMethod/exe_16102024/Lab_014_InputsInJavaUsingARGS.java
@@ -0,0 +1,19 @@
+package Oct_Exercise_mainMethod.exe_16102024;
+
+public class Lab_014_InputsInJavaUsingARGS {
+    public static void main(String[] args) { //basically its a string so we need to parse it
+
+        // to integer to take as an input from user if its integer
+
+        System.out.printf("The First no in args is: " + "%s" + '\n' + "The second no in args is: " + "%s", args[0], args[1]);
+        System.out.println();
+        //System.out.println("The max is:" + ((args[0] > args[1] ) ? args[0] : args[1]));//this is not valid as the args are in string format
+        int num1 = Integer.parseInt(args[0]); //needs to be parsed from string to int before using it
+        int num2 = Integer.parseInt(args[1]);
+        System.out.println("The Max of two args is:" + ((num1 > num2) ? num1 : num2));
+
+
+
+
+    }
+}
---
+++ src/Oct_Exercise_mainMethod/exe_16102024/Lab_015_InputsScanner.java
@@ -0,0 +1,16 @@
+package Oct_Exercise_mainMethod.exe_16102024;
+import java.util.Scanner;
+public class Lab_015_InputsScanner {
+    public static void main(String[] args) {
+
+        Scanner s1 = new Scanner(System.in);
+        System.out.println("Enter the Name:");
+        String name =  s1.nextLine();
+        System.out.println("Enter the Age:");
+        byte age = s1.nextByte();
+        System.out.println("Enter the salary:");
+        float sal = s1.nextFloat();
+
+        System.out.println("The details entered are:" + '\n' + "Employee Name: " + name + '\n' + "Employee Age: " + age + '\n' + "Employee Salary: " + sal  );
+        }
+}
