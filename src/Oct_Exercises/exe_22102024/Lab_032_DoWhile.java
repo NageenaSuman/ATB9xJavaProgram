82e47164b14ee772c4c265bd8aee94c20e46c7f7
NageenaSuman <kaunaineee@gmail.com>
2024-10-23 16:30:25 +0800

    today's update

---
+++ src/Oct_Exercises/exe_22102024/Lab_032_DoWhile.java
@@ -0,0 +1,11 @@
+package Oct_Exercises.exe_22102024;
+
+public class Lab_032_DoWhile {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println("Executing the body");
+            i++;
+            }
+        while (i >= 10) ;
+}}
