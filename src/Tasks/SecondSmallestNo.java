b16dd20dcd5a588912dfa8a2aaf65f15814873ee
NageenaSuman <kaunaineee@gmail.com>
2025-01-01 21:44:25 +0800

    updated

--- src/Tasks/SecondSmallestNo.java
+++ src/Tasks/SecondSmallestNo.java
@@ -2,30 +2,32 @@ package Tasks;
 
 import java.util.Scanner;
 
-public class SecondLargestNo {
-
+public class SecondSmallestNo {
     public static void main(String[] args) {
         System.out.println("Enter the size of an array to find second largest of it:");
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
         int[] num = new int[size];
-        int ch = num[0];
+
         System.out.println("Enter the numbers:");
-        for (int i = 0; i < size; i++) {
+        for (int i=0;i<size;i++) {
             num[i] = sc.nextInt();
-            if (num[i] > ch) {
+        }
+        int ch = num[0];
+        for (int i=0;i<size;i++){
+        if (num[i] < ch) {
                 ch = num[i];
             }
         }
-        System.out.println("First largest is:" + ch);
+        System.out.println("First smallest is:" + ch);
         int ch1 = num[0];
         for (int i = 0; i < size; i++) {
 
-            if (num[i] < ch && ch1 < num[i]) {
+            if (num[i] > ch && ch1 > num[i]) {
                 ch1 = num[i];
             }
 
 
         }
-        System.out.println("Second largest is:" + ch1);
+        System.out.println("Second smallest is:" + ch1);
     }}
